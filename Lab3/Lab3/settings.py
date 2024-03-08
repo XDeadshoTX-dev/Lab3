36861e2e94b0a1ea2013901fd5f47ec2a33fec0e
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-03-08 13:34:54 +0200

    Updated

--- Lab3/Lab3/settings.py
+++ Lab3/Lab3/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import posixpath
+from Lab3.my_secret import MySecret
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e99170a2-127b-4285-8c54-fac109552ec8'
+SECRET_KEY = MySecret()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
