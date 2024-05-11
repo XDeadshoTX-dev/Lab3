66302fa0fe49d4ed533bbbe97ba1d9ff19b50100
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-05-11 18:59:05 +0300

    Example of GET, POST in views (controller), and redirect urls with templates (index.html)

--- Lab3/Lab3/urls.py
+++ Lab3/Lab3/urls.py
@@ -27,4 +27,6 @@ urlpatterns = [
          name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
+    path('get_number/', views.GetNumberView.as_view(), name='get_number'),
+    path('post_name/', views.PostNameView.as_view(), name='post_name'),
 ]
--- Lab3/app/views.py
+++ Lab3/app/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views import View
 """
 Definition of views.
 """
@@ -17,6 +20,26 @@ def home(request):
             'year':datetime.now().year,
         }
     )
+
+class GetNumberView(View):
+    def get(self, request, *args, **kwargs):
+        if request.method == "GET":
+            number = request.GET.get('number', None)
+            return HttpResponse(f"number is {number}")
+        # if number is not None and number.isdigit() and int(number) == 10:
+        #     return JsonResponse({"status": "success", "message": "Number is 10"})
+        # else:
+        #     return JsonResponse({"status": "error", "message": "Number is not 10"})
+        
+class PostNameView(View):
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name', None)
+        if name is not None:
+            return JsonResponse({"status": "success", "message": f"Hello, {name}"})
+        else:
+            return JsonResponse({"status": "error", "message": "Name is not provided"})
+        
+
 
 def contact(request):
     """Renders the contact page."""
