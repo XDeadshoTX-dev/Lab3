1c036db32be8cf2ac89cc324422ba2be7b7107f6
XDeadshoTX <Kapitan.rex2015@gmail.com>
2024-05-17 12:12:53 +0300

    Added allowed host

--- Lab3/Lab3/settings.py
+++ Lab3/Lab3/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = MySecret()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["myhostua.ddns.net"]
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
